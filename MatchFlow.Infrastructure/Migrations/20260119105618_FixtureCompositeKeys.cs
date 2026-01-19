a35a41d8f762a78385558d61797bf03ae41f6063
ReesMehorst <24141720@student.hhs.nl>
2026-01-19 12:06:54 +0100

    Migration voor de composite keys van participant en team (fixture)

---
+++ MatchFlow.Infrastructure/Migrations/20260119105618_FixtureCompositeKeys.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFlow.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixtureCompositeKeys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
